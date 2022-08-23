b8a17cf73b03e48ff0ae3274466d62d0af1b61ca
peters04 <piotreksmolinski04@gmail.com>
2022-08-23 21:31:06 +0200

    whatever

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const { DateTime } = require('luxon');
+
+module.exports = (eleventyConfig) => {
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter('postDate', dateObj => DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED))
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+}
